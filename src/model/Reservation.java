e9ba876039845b55f292e23a34cf0e5b3dbfe70a
sonalikatara <sonalikatara@gmail.com>
2021-05-16 14:41:49 -0700

    override the equals() and hashcode() methods in Reservation model

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -55,4 +55,25 @@ public class Reservation {
                 "Checkout date : " + checkOutDate;
 
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) { return true; }
+        if (obj == null || obj.getClass() != this.getClass()) { return false; }
+        Reservation reservation = (Reservation) obj;
+        return customer == reservation.customer && (room == reservation.room || (room != null && room.equals(reservation.getRoom()))) && (checkInDate == reservation.getCheckInDate() || (checkInDate != null && checkInDate.equals(reservation.getCheckInDate()))) && (checkOutDate == reservation.getCheckOutDate() || (checkOutDate != null && checkOutDate.equals(reservation.getCheckOutDate())));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+        result = prime * result + ((room == null) ? 0 : room.hashCode());
+        result = prime * result + ((checkInDate == null) ? 0 : checkInDate.hashCode());
+        result = prime * result + ((checkOutDate == null) ? 0 : checkOutDate.hashCode());
+        return result;
+    }
+
+
 }
